eaea1b577d8693a7a58a1c1c2f6d310a2551f7a0
Polyxen1a <maslowa@inbox.ru>
2023-07-16 22:48:27 +0300

    Initial commit

---
+++ src/main/java/pro/sky/coursepapertesttechnicalinterview/CoursePaperTestTechnicalInterviewApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.coursepapertesttechnicalinterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursePaperTestTechnicalInterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursePaperTestTechnicalInterviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/coursepapertesttechnicalinterview/CoursePaperTestTechnicalInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.coursepapertesttechnicalinterview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursePaperTestTechnicalInterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
